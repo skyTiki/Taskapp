49a5d8a060e540cfcb9165629e8d34a1cf1e80ab
takatoshi.ichige <takatoshi.ichige>
2021-03-22 12:01:39 +0900

    Category取得部分をRealmで記載

--- taskapp/ViewControllers/CategoryListViewController.swift
+++ taskapp/ViewControllers/CategoryListViewController.swift
@@ -6,17 +6,23 @@
 //
 
 import UIKit
+import RealmSwift
 
 class CategoryListViewController: UIViewController {
 
     @IBOutlet weak var categoryTableView: UITableView!
     
-    var categoryList = ["家事", "勉強"]
-    var selectedCategoryList: [String] = []
+    // Realmから読み取り
+    let realm = try! Realm()
+    var categoryList: Results<Category>!
     
+    // 設定されたCategoryを設定する
+    var selectedCategoryList: [Category] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        categoryList = realm.objects(Category.self)
         
         categoryTableView.delegate = self
         categoryTableView.dataSource = self
@@ -31,20 +37,20 @@ extension CategoryListViewController: UITableViewDelegate, UITableViewDataSource
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
-        cell.textLabel?.text = categoryList[indexPath.row]
+        cell.textLabel?.text = categoryList[indexPath.row].name
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let cell = tableView.cellForRow(at: indexPath) else { return }
-        guard let selectedCategoryTitle = cell.textLabel!.text else { return }
+        guard let selectedCategory: Category = categoryList.first(where: { $0.name == cell.textLabel?.text }) else { return }
         
         if cell.accessoryType == .none {
             cell.accessoryType = .checkmark
-            selectedCategoryList.append(selectedCategoryTitle)
+            selectedCategoryList.append(selectedCategory)
         } else if cell.accessoryType == .checkmark {
             cell.accessoryType = .none
-            selectedCategoryList.removeAll(where: { $0 == selectedCategoryTitle })
+            selectedCategoryList.removeAll(where: { $0 == selectedCategory })
         }
         
         tableView.deselectRow(at: indexPath, animated: true)
